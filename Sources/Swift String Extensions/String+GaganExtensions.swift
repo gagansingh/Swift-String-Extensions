56ce51c51acc5af454fead45ec855b25f66eccdd
Gagan Singh <gsingh@resy.com>
2020-05-17 14:11:32 -0400

    functionality, plus adding tests

--- Sources/Swift String Extensions/String+GaganExtensions.swift
+++ Sources/Swift String Extensions/String+GaganExtensions.swift
@@ -12,13 +12,23 @@ enum GaganExtensionsError: Error {
 
 extension String {
     func replacingNonLettersWith(_ replacement: String) throws -> String {
-        guard self.count == 1 else { throw GaganExtensionsError.runtimeError("replacement string much be one character long") }
+        guard replacement.count == 1 else { throw GaganExtensionsError.runtimeError("replacement string much be one character long") }
         let c = Character(replacement)
         return replacingNonLettersWith(c)
     }
 
     func replacingNonLettersWith(_ replacement: Character) -> String {
-        let allowedCharacters = CharacterSet.letters
+        return replacingCharacters(inSet: CharacterSet.letters, with: replacement)
+    }
+
+    func replacingCharacters(inSet set: CharacterSet, with replacement: String) throws -> String {
+        guard replacement.count == 1 else { throw GaganExtensionsError.runtimeError("replacement string much be one character long") }
+        let c = Character(replacement)
+        return replacingCharacters(inSet: set, with: c)
+    }
+
+    func replacingCharacters(inSet set: CharacterSet, with replacement: Character) -> String {
+        let allowedCharacters = set 
         let newString = self.map { (c) -> Character in
             let characterIsNotAllowed = { !allowedCharacters.contains($0) }
             let charactersUnicodeScalarsContainsNotAllowed = c.unicodeScalars.contains(where: characterIsNotAllowed )
--- Tests/Swift String ExtensionsTests/Swift_String_ExtensionsTests.swift
+++ Tests/Swift String ExtensionsTests/Swift_String_ExtensionsTests.swift
@@ -2,14 +2,20 @@ import XCTest
 @testable import Swift_String_Extensions
 
 final class Swift_String_ExtensionsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Swift_String_Extensions().text, "Hello, World!")
+    func testNonLettersReplacement() {
+        let string = "thx!lol@44"
+        let test = try! string.replacingNonLettersWith("_")
+        XCTAssertEqual(test, "thx_lol___")
+    }
+
+    func testNonAlphaNumericReplacement() {
+        let string = "thx!lol@44"
+        let test = try! string.replacingCharacters(inSet: CharacterSet.alphanumerics, with: "_")
+        XCTAssertEqual(test, "thx_lol_44")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testReplacement", testNonLettersReplacement),
+        ("testNonAlphaNumericReplacement", testNonAlphaNumericReplacement)
     ]
 }
